087e58fca4b71e9e9dedc01e52facca29daa4336
Margus <margus.liinev@gmail.com>
2023-10-17 22:15:53 +0300

    added typechecking, linting checks and optimized ci flow

--- apps/client/src/App.tsx
+++ apps/client/src/App.tsx
@@ -1,15 +1,15 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 
 function App() {
-    const [greeting, setGreeting] = useState('');
+  const [greeting, setGreeting] = useState("");
 
-    useEffect(() => {
-        fetch('/api/v1')
-            .then((res) => res.text())
-            .then(setGreeting);
-    }, []);
+  useEffect(() => {
+    fetch("/api/v1")
+      .then((res) => res.text())
+      .then(setGreeting);
+  }, []);
 
-    return <main>{greeting}</main>;
+  return <main>{greeting}</main>;
 }
 
 export default App;
--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -1,9 +1,9 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
 
 async function bootstrap() {
-    const app = await NestFactory.create(AppModule);
-    app.setGlobalPrefix('/api/v1');
-    await app.listen(process.env.PORT || 5000);
+  const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix("/api/v1");
+  await app.listen(process.env.PORT || 5000);
 }
-bootstrap();
+void bootstrap();
