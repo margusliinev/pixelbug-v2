a189be87c5879592f2bd74f5ac9947695a5a8bfa
Margus <margus.liinev@gmail.com>
2023-10-18 21:41:06 +0300

    added fonts, favicon and other public files, tailwind & shadcn setup

--- apps/client/src/App.tsx
+++ apps/client/src/App.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react';
+import { Button, Input, Label } from './components/ui';
 
 function App() {
     const [greeting, setGreeting] = useState('');
@@ -12,7 +13,26 @@ function App() {
             });
     }, []);
 
-    return <main>{greeting}</main>;
+    return (
+        <main className='grid h-screen w-screen place-items-center'>
+            <form className='grid w-full max-w-md gap-3 rounded-md border bg-white p-6 shadow-sm'>
+                <h1 className='my-4 text-center text-lg font-medium tracking-tight'>{greeting}</h1>
+                <fieldset>
+                    <Label htmlFor='username'>Username</Label>
+                    <Input type='text' name='username' id='username'></Input>
+                </fieldset>
+                <fieldset>
+                    <Label htmlFor='email'>Email</Label>
+                    <Input type='email' name='email' id='email'></Input>
+                </fieldset>
+                <fieldset>
+                    <Label htmlFor='password'>Password</Label>
+                    <Input type='password' name='password' id='password'></Input>
+                </fieldset>
+                <Button type='submit'>Submit</Button>
+            </form>
+        </main>
+    );
 }
 
 export default App;
---
+++ apps/client/tailwind.config.js
@@ -0,0 +1,120 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    darkMode: ['class'],
+    content: ['./pages/**/*.{ts,tsx}', './components/**/*.{ts,tsx}', './app/**/*.{ts,tsx}', './src/**/*.{ts,tsx}'],
+    theme: {
+        fontFamily: {
+            Inter: ['Inter', 'Inter Fallback'],
+        },
+        container: {
+            center: true,
+            padding: '2rem',
+            screens: {
+                '2xl': '1400px',
+            },
+        },
+        extend: {
+            backgroundImage: {
+                pattern: "url('/background.png')",
+            },
+            screens: {
+                xs: '600px',
+                xxs: '500px',
+            },
+            width: {
+                'screen-90': '90vw',
+                'screen-80': '80vw',
+                'screen-70': '70vw',
+                'screen-60': '60vw',
+                'screen-50': '50vw',
+                'screen-40': '40vw',
+                'screen-30': '30vw',
+                'screen-20': '20vw',
+                'screen-10': '10vw',
+            },
+            height: {
+                'screen-90': '90vh',
+                'screen-80': '80vh',
+                'screen-70': '70vh',
+                'screen-60': '60vh',
+                'screen-50': '50vh',
+                'screen-40': '40vh',
+                'screen-30': '30vh',
+                'screen-20': '20vh',
+                'screen-10': '10vh',
+            },
+            gridTemplateColumns: {
+                'sidebar-layout': 'auto 1fr',
+            },
+            minHeight: {
+                'screen-minus-nav': 'calc(100vh - 4rem)',
+            },
+            boxShadow: {
+                'sm-right': '1px 0 2px 0 rgba(0,0,0,0.05)',
+            },
+            maxWidth: {
+                xxs: '16rem',
+            },
+            colors: {
+                border: 'hsl(var(--border))',
+                input: 'hsl(var(--input))',
+                ring: 'hsl(var(--ring))',
+                background: 'hsl(var(--background))',
+                foreground: 'hsl(var(--foreground))',
+                primary: {
+                    DEFAULT: 'hsl(var(--primary))',
+                    foreground: 'hsl(var(--primary-foreground))',
+                    hover: 'hsl(var(--primary-hover))',
+                },
+                destructive: {
+                    DEFAULT: 'hsl(var(--destructive))',
+                    foreground: 'hsl(var(--destructive-foreground))',
+                    hover: 'hsl(var(--destructive-hover))',
+                },
+                secondary: {
+                    DEFAULT: 'hsl(var(--secondary))',
+                    foreground: 'hsl(var(--secondary-foreground))',
+                    hover: 'hsl(var(--secondary-hover))',
+                },
+                muted: {
+                    DEFAULT: 'hsl(var(--muted))',
+                    foreground: 'hsl(var(--muted-foreground))',
+                    hover: 'hsl(var(--muted-hover))',
+                },
+                accent: {
+                    DEFAULT: 'hsl(var(--accent))',
+                    foreground: 'hsl(var(--accent-foreground))',
+                    hover: 'hsl(var(--accent-hover))',
+                },
+                popover: {
+                    DEFAULT: 'hsl(var(--popover))',
+                    foreground: 'hsl(var(--popover-foreground))',
+                },
+                card: {
+                    DEFAULT: 'hsl(var(--card))',
+                    foreground: 'hsl(var(--card-foreground))',
+                },
+            },
+            borderRadius: {
+                lg: 'var(--radius)',
+                md: 'calc(var(--radius) - 2px)',
+                sm: 'calc(var(--radius) - 4px)',
+            },
+            keyframes: {
+                'accordion-down': {
+                    from: { height: 0 },
+                    to: { height: 'var(--radix-accordion-content-height)' },
+                },
+                'accordion-up': {
+                    from: { height: 'var(--radix-accordion-content-height)' },
+                    to: { height: 0 },
+                },
+            },
+            animation: {
+                'accordion-down': 'accordion-down 0.2s ease-out',
+                'accordion-up': 'accordion-up 0.2s ease-out',
+            },
+        },
+    },
+    plugins: [require('tailwindcss-animate')],
+};
